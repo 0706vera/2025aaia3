a55ebf3386042cb3731e7d3621be28e41be72844
0706vera <vera0706123456@gmail.com>
2025-09-25 17:18:50 +0800

    week03

---
+++ week03/week03-3a.cpp
@@ -0,0 +1,16 @@
+///week03-3a.cpp 二合一的第1種方法 把不是0的找出來、暫放ans，再放回去
+///LeetCode 學習計畫 283. Move Zeroes
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] != 0)ans.push_back(nums[i]);
+        }
+        //再用 for 迴圈放回去
+        for(int i=0; i<nums.size();i++){
+            if(i < ans.size()) nums[i] = ans[i];//塞回去
+            else nums[i] = 0;//其他放0
+        }
+    }
+};
